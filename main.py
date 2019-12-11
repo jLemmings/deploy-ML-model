f7b6c7ba2be56be0be93e66bde9bc29829711781
jLemmings <josh@hemmings.ch>
2019-12-11 11:08:36 +0100

    updated flask port

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print("STARTING WEBSERVER PORT: ", port)
-    app.run(threaded=True, port=port)
+    app.run(threaded=True)
