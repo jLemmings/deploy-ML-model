fe6ceb255d2663ecc592cc82dd483e907e3d3c1f
jLemmings <josh@hemmings.ch>
2019-12-15 12:43:25 +0100

    update host port

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 33507))
     print("STARTING WEBSERVER PORT: ", port)
-    app.run(host='127.0.0.1', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port, threaded=True)
